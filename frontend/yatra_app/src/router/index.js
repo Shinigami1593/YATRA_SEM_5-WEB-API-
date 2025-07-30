b6b73c94d1ac6639b52a8b780e8c455d8124fba4
ayush <rasaileeayush17@gmail.com>
2025-07-31 04:47:25 +0545

    final router of frontend

--- frontend/yatra_app/src/router/index.js
+++ frontend/yatra_app/src/router/index.js
@@ -5,7 +5,15 @@ import Login from '../views/Auth/SignIn.vue';
 import Register from '../views/Auth/SignUp.vue';
 import Home from '@/views/Home.vue';
 import AdminDashBoard from '@/views/Admin/AdminDashBoard.vue';
-import AddRoute from '@/views/Admin/AddRoute.vue';
+import AddTrip from '@/views/Admin/Trip/AddTrip.vue';
+import EditTrip from '@/views/Admin/Trip/EditTrip.vue';
+import RoutesDetails from '@/views/Routes/RoutesDetails.vue';
+import Profile from '@/views/Profile.vue';
+import AdminRoutes from '@/views/Admin/Routes/AdminRoutes.vue';
+import Routes from '@/views/Routes/Routes.vue';
+import MapView from '@/components/MapView.vue';
+// import RoutesDetails from '@/views/Routes/RoutesDetails.vue';
+// import AddRoute from '@/views/Admin/AddRoute.vue';
 
 const routes = [
   {
@@ -15,9 +23,33 @@ const routes = [
     meta: { requiresAuth: true },
   },
   {
+    path: '/user/all-routes',
+    name: 'Routes',
+    component: Routes,
+    meta: { requiresAuth: true },
+  },
+  {
+    path: '/profile',
+    name: 'Profile',
+    component: Profile,
+    meta: { requiresAuth: true },
+    
+  },
+  {
+    path: '/details-map',
+    name: 'Route Details',
+    component: RoutesDetails,
+    meta: { requiresAuth: true },
+  },
+  {
     path: '/',
     name: 'Login',
     component: Login,
+  },
+  {
+    path: '/map',
+    name: 'MapView',
+    component: MapView,
   },
   {
     path: '/register',
@@ -31,9 +63,21 @@ const routes = [
     meta: { requiresAuth: true, requiresAdmin: true },
   },
   {
-    path: '/admin/addRoute',
-    name: 'routeAdd',
-    component: AddRoute,
+    path: '/admin/add-trip',
+    name: 'TripAdd',
+    component: AddTrip,
+    meta: { requiresAuth: true, requiresAdmin: true },
+  },
+  {
+    path: '/admin/route',
+    name: 'Route',
+    component: AdminRoutes,
+    meta: { requiresAuth: true, requiresAdmin: true },
+  },
+  {
+    path: '/admin/edit-trip/:id',
+    name: 'TripEdit',
+    component: EditTrip,
     meta: { requiresAuth: true, requiresAdmin: true },
   },
 ];
